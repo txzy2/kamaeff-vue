b12736c8ce6e5adc2d00b8528153747b70bbae6c
Anton Kamaev <99755906+kamaeff@users.noreply.github.com>
2024-09-21 20:42:43 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kamaeff-vue/sw.js', { scope: '/kamaeff-vue/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
